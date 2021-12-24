872908bf4f5cc8a3205b2dd9b0570fa718741474
leonardo-pinto <leonardoan.pinto@gmail.com>
2021-12-24 00:26:30 -0300

    fim curso

---
+++ part01/app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+    adicionar(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    };
+
+    listar(): readonly Negociacao[] {
+        return this.negociacoes;
+    }
+}
---
+++ part01/dist/js/models/negociacoes.js
@@ -0,0 +1,12 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adicionar(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    ;
+    listar() {
+        return this.negociacoes;
+    }
+}
